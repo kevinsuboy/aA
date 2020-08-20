8b7f2a798d1c6565d6838f9729f2b673eb443735
kevinsuboy <kevin.suboy@gmail.com>
2020-08-19 17:07:50 -0700

    chess w4d3

--- w4/d2/chess/lib/Piece/NullPiece.rb
+++ w4/d2/chess/lib/Piece/NullPiece.rb
@@ -3,5 +3,13 @@ require 'singleton'
 
 class NullPiece < Piece
     include Singleton # is this enuff?
+    def initialize
+        @symbol = "."
+        # @symbol = "_|"
+        @color = :none
+    end
+    def moves
+        []
+    end
 end
-                        
+                                            
