a44188205f48c2922f7c0a91dbf820d344fdbba8
kevinsuboy <kevin.suboy@gmail.com>
2020-10-07 14:23:09 -0700

    done with frontend, pre-BnB

---
+++ w11/d3/frontend_auth/frontend/utils/session.js
@@ -0,0 +1,22 @@
+import { $CombinedState } from "redux"
+
+export const postUser = user => (
+    $.ajax({
+        url: '/api/users',
+        method: 'POST',
+        data: { user },
+    })
+);
+export const postSession = user => (
+    $.ajax({
+        url: '/api/session',
+        method: 'POST',
+        data: { user },
+    })
+);
+export const deleteSession = () => (
+    $.ajax({
+        url: '/api/session',
+        method: 'DELETE',
+    })
+);
